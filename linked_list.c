cdd9b6e00337fd5d6c173f04178114e072a0510c
Tobbe821 <34543773+Globose@users.noreply.github.com>
2024-09-30 17:45:33 +0200

    brief

--- linked_list.c
+++ linked_list.c
@@ -1,19 +1,35 @@
 #include "linked_list.h"
 
-
+/**
+ * @brief  Creates a new node
+ * @param  data     Data to be stored
+ * @param  next     Pointer to the next node
+ * @return Node*    Pointer to new node
+ */
 Node* get_new_node(int data, Node* next){
     Node*new_node = (Node*)mem_alloc(sizeof(Node));
     *new_node = (Node){data,next};
     return new_node;
 }
 
-// This function sets up the list and prepares it for operations.
+/**
+ * @brief  This function sets up the list and prepares it for operations
+ * @param  head     Pointer to pointer to head
+ * @param  size     Size of memory pool
+ * @note   - If head is NULL, nothing happens
+ */
 void list_init(Node** head, size_t size){
+    if (head == NULL) return;
     mem_init(size);
     *head = NULL;
 }
 
-// Adds a new node with the specified data to the linked list. 
+/**
+ * @brief  Adds a new node with the specified data to the linked list
+ * @param  head     Pointer to pointer to head
+ * @param  data     Data to be inserted
+ * @note   - If head is NULL, nothing happens
+ */
 void list_insert(Node** head, int data){
     if (head == NULL) return;
     if (*head == NULL){
@@ -28,13 +44,25 @@ void list_insert(Node** head, int data){
     }
 }
 
-// Inserts a new node with the specified data immediately after a given node.
+/**
+ * @brief  Inserts a new node with the specified data immediately after a given node
+ * @param  prev_note    Pointer to previous node
+ * @param  data         Data to be inserted
+ * @note   - If prev_node is NULL, nothing happens
+ */
 void list_insert_after(Node* prev_node, int data){
+    if (prev_node == NULL) return;
     Node* new_node = get_new_node(data, prev_node->next);
     prev_node->next = new_node;
 }
 
-// Inserts a new node with the specified data immediately before a given node in the list. 
+/**
+ * @brief  Inserts a new node with the specified data immediately before a given node in the list
+ * @param  head     Pointer to pointer to head
+ * @param  next_node    Pointer to next node
+ * @param  data     Data to be inserted
+ * @note   - If head is NULL or next_node doesn't exist, nothing happens
+ */
 void list_insert_before(Node** head, Node* next_node, int data){
     if (head == NULL) return;
     Node* prev_node = NULL;
@@ -57,7 +85,12 @@ void list_insert_before(Node** head, Node* next_node, int data){
     } 
 }
 
-// Removes a node with the specified data from the linked list.
+/**
+ * @brief  Removes all nodes with the specified data from the linked list.
+ * @param  head     Pointer to pointer to head
+ * @param  data     Data value to be deleted
+ * @note   - If head is NULL, nothing happens
+ */
 void list_delete(Node** head, int data){
     if (head == NULL) return;
     Node* prev_node = NULL;
@@ -81,7 +114,14 @@ void list_delete(Node** head, int data){
     } 
 }
 
-// Searches for a node with the specified data and returns a pointer to it.
+/**
+ * @brief  Searches for a node with the specified data and returns a pointer to it
+ * @param  head     Pointer to pointer to head
+ * @param  data     Data to search for
+ * @return Node*    Pointer to search result
+ * @note   - If head is NULL, NULL is returned.
+ *         - If no node with the given value is found, NULL is returned.
+ */
 Node* list_search(Node** head, int data){
     if (head == NULL) return NULL;
     Node*node = *head;
@@ -91,8 +131,17 @@ Node* list_search(Node** head, int data){
     return node;
 }
 
-// Prints all the elements in the linked list as a list separated by commas, enclosed in square brackets.
+/**
+ * @brief  Prints all the elements in a given range
+ * @param  head     Pointer to pointer to head
+ * @param  Node*    First node to display
+ * @param  Node*    Last node to display
+ * @note   - If head is NULL, nothing happens.
+ *         - If start_node is NULL, displaying will begin with the first node in the list.
+ *         - If end_node is NULL, displaying will end with the last node in the list.
+ */
 void list_display_range(Node** head, Node* start_node, Node* end_node){
+    if (head == NULL) return;
     printf("[");
     if (start_node == NULL) {
         start_node = *head;
@@ -106,12 +155,23 @@ void list_display_range(Node** head, Node* start_node, Node* end_node){
     printf("]");
 }
 
+/**
+ * @brief  Prints all the elements in the linked list
+ * @param  head     Pointer to pointer to head
+ * @note   - If head is NULL, nothing happens.
+ */
 void list_display(Node** head){
+    if (head == NULL) return;
     list_display_range(head, *head, NULL);
 }
 
-// Returns the count of nodes
+/**
+ * @brief  Returns the count of nodes
+ * @param  head     Pointer to pointer to head
+ * @note   - If head is NULL, -1 will be returned.
+ */
 int list_count_nodes(Node** head){
+    if (head == NULL) return -1;
     Node* node = *head;
     int counter = 0;
     while (node != NULL){
@@ -121,8 +181,13 @@ int list_count_nodes(Node** head){
     return counter;
 }
 
-// Frees all the nodes in the linked list. Important to prevent memory leaks.
+/**
+ * @brief  Frees all the nodes in the linked list
+ * @param  head     Pointer to pointer to head
+ * @note   - If head is NULL, nothing happens.
+ */
 void list_cleanup(Node** head){
+    if (head == NULL) return;
     Node* node = *head;
     while (node != NULL){
         Node* old_node = node;
--- memory_manager.c
+++ memory_manager.c
@@ -12,6 +12,13 @@ static void * m_block;
 static size_t m_size;
 static struct MemoryBlock * first_block;
 
+/**
+ * @brief  Creates a new memory block and metadata for it
+ * @param  ptr  Pointer to the first memory block.
+ * @param  block2   Pointer to the second memory block.
+ * @return MemoryBlock*     Pointer to the new memory block.
+ * @note    - Only merges if both memory blocks are free.
+ */
 struct MemoryBlock* add_mem(void* ptr, size_t size, int free, struct MemoryBlock* prev, struct MemoryBlock* next){
     if (size == 0) return next;
     struct MemoryBlock* new_block = (struct MemoryBlock*)malloc(sizeof(struct MemoryBlock));
@@ -21,6 +28,11 @@ struct MemoryBlock* add_mem(void* ptr, size_t size, int free, struct MemoryBlock
     return new_block;
 }
 
+/**
+ * @brief  Splits one memory block into two
+ * @param  current_block    Pointer to the memory block.
+ * @param  size     Size of the first part of the memory block.
+ */
 void split_mem(struct MemoryBlock* current_block, size_t size) {
     size_t new_size = current_block->size-size;
     current_block->free = 0;
@@ -29,6 +41,12 @@ void split_mem(struct MemoryBlock* current_block, size_t size) {
     current_block->next = add_mem(next_ptr, new_size, 1, current_block, current_block->next);
 }
 
+/**
+ * @brief  Attempts to merge two memory blocks
+ * @param  block1   Pointer to the first memory block.
+ * @param  block2   Pointer to the second memory block.
+ * @note    - Only merges if both memory blocks are free.
+ */
 void merge_mem(struct MemoryBlock* block1, struct MemoryBlock* block2){
     if (block1 == NULL || block2 == NULL) return;
     if (block1->free == 0 || block2->free == 0) return;
@@ -37,6 +55,13 @@ void merge_mem(struct MemoryBlock* block1, struct MemoryBlock* block2){
     if (block2->next != NULL) block2->next->prev = block1;
     free(block2);
 }
+
+/**
+ * @brief  Searches for a matching memory block
+ * @param  block     Pointer to the memory block ptr.
+ * @return MemoryBlock*     Pointer to the memory block.
+ * @note    - Returns NULL if no matching block is found.
+ */
 
 struct MemoryBlock* get_block(void* block){
     struct MemoryBlock * current_block = first_block;
@@ -47,7 +72,12 @@ struct MemoryBlock* get_block(void* block){
     return current_block;
 }
 
-// Initializes the memory manager with a specified size of memory pool. 
+/**
+ * @brief  Initializes the memory manager with a specified size of memory pool.
+ * @param  size     Size of memory pool.
+ * @note
+ * - If size <0, no pool will be allocated. If size=0 an empty pool will be created.
+ */
 void mem_init(size_t size){
     if (size < 0) return;
     m_block = malloc(size);
@@ -55,7 +85,13 @@ void mem_init(size_t size){
     first_block = add_mem(m_block, m_size, 1, NULL, NULL);
 }
 
-// Allocates a block of memory of the specified size. Returns poitiers  
+/**
+ * @brief  Allocates a block of memory of the specified size.
+ * @param  size Size of memory block.
+ * @return void*    A pointer to the block.
+ * @note
+ * - If memory allocation fails it will return NULL
+ */
 void* mem_alloc(size_t size){
     size = (size+7)&~7;
     struct MemoryBlock* current_block = first_block;
@@ -67,7 +103,12 @@ void* mem_alloc(size_t size){
     return current_block->ptr;
 }
 
-// Frees the specified block of memory
+/**
+ * @brief  Frees the specified block of memory
+ * @param  block    Pointer to memory block.
+ * @note
+ * - Does nothing if block is not in the pool.
+ */
 void mem_free(void* block){
     struct MemoryBlock * current_block = get_block(block);
     if (current_block == NULL) return;
@@ -76,7 +117,15 @@ void mem_free(void* block){
     merge_mem(current_block->prev, current_block);
 }
 
-// Changes the size of the memory block, possibly moving it. 
+/**
+ * @brief  Changes the size of the memory block, possibly moving it. 
+ * @param  block    Pointer to memory block.
+ * @return void*    A pointer to the block.
+ * @note
+ * - Does nothing if block is not in the pool.
+ * - Returns NULL if not enough memory is available in the pool. While the memory block remains unchanged.
+ */
+
 void* mem_resize(void* block, size_t size){
     size = (size+7)&~7;
     struct MemoryBlock* current_block = get_block(block);
@@ -101,7 +150,10 @@ void* mem_resize(void* block, size_t size){
     }
 }
 
-// Frees up the memory pool
+
+/**
+ * @brief  Frees up the memory pool
+ */
 void mem_deinit(){
     struct MemoryBlock* current_block = first_block;
     while (current_block != NULL){
